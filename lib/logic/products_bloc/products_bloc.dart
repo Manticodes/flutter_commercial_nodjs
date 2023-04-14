2f69f92aeeb1668eb75b0eeb7eb81a9f811d5721
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-04-14 14:36:50 +0200

    fix product bloc

--- lib/logic/products_bloc/products_bloc.dart
+++ lib/logic/products_bloc/products_bloc.dart
@@ -1,12 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_commercial_nodjs/model/product.dart';
 
 part 'products_event.dart';
 part 'products_state.dart';
 
 class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
-  ProductsBloc() : super(ProductsInitial(products: [])) {
+  ProductsBloc() : super(ProductsInitial()) {
     on<ProductsEvent>((event, emit) {
       // TODO: implement event handler
     });
