b35322415161c9d3aaa25590e218d322a0125b08
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-04-25 23:52:17 +0330

    bettering search page

--- lib/features/search/widget/search_widgets.dart
+++ lib/features/search/widget/search_widgets.dart
@@ -16,54 +16,55 @@ class SearchWidgets extends StatelessWidget {
             height: 200,
             width: double.infinity,
             child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Image.network(
-                products[index].images[0],
-                width: MediaQuery.of(context).size.width * 0.4,
-                height: 200,
-                fit: BoxFit.cover,
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  products[index].images[0],
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
               ),
               Flexible(
-                child: Container(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15.0, top: 8),
-                          child: Text(
-                            product.name,
-                            overflow: TextOverflow.clip,
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w500),
-                          ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0, top: 8),
+                        child: Text(
+                          product.name,
+                          overflow: TextOverflow.clip,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15.0, top: 8),
-                          child: Text(
-                            product.description,
-                            overflow: TextOverflow.clip,
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500),
-                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0, top: 8),
+                        child: Text(
+                          product.description,
+                          overflow: TextOverflow.clip,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 13.0, top: 8),
-                          child: Text.rich(TextSpan(
-                            children: [
-                              WidgetSpan(
-                                  child: Icon(
-                                Icons.attach_money_outlined,
-                                size: 20,
-                                color: Colors.green,
-                              )),
-                              WidgetSpan(child: Text(product.price.toString())),
-                              WidgetSpan(child: Text('  تومان'))
-                            ],
-                          )),
-                        ),
-                      ]),
-                ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13.0, top: 8),
+                        child: Text.rich(TextSpan(
+                          children: [
+                            const WidgetSpan(
+                                child: Icon(
+                              Icons.attach_money_outlined,
+                              size: 20,
+                              color: Colors.green,
+                            )),
+                            WidgetSpan(child: Text(product.price.toString())),
+                            const WidgetSpan(child: Text('  تومان'))
+                          ],
+                        )),
+                      ),
+                    ]),
               )
             ]),
           ),
