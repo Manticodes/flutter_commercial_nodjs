d856bb7d2ca872f5e88c0a1ec9169791bf2229b0
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-04-25 21:32:05 +0330

    fix server

--- server/routes/products.js
+++ server/routes/products.js
@@ -16,7 +16,7 @@ productRouter.get('/api/get-products', auth, async (req, res) => {
 });
 
 
-productRouter.get('/api/get-products/:name', auth, async (req, res) => {
+productRouter.get('/api/get-products/search/:name', auth, async (req, res) => {
     try {
 
         const products = await Product.find({ name: { $regex: req.params.name, $options: "i" }, });
