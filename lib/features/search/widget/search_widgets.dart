316033b95d24493109e17024e3c5666ffd1b994a
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-04-25 23:42:19 +0330

    add price and description

--- lib/features/search/widget/search_widgets.dart
+++ lib/features/search/widget/search_widgets.dart
@@ -9,12 +9,12 @@ class SearchWidgets extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
+        Product product = products[index];
         return Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
             height: 200,
             width: double.infinity,
-            color: Colors.amber,
             child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
               Image.network(
                 products[index].images[0],
@@ -24,23 +24,52 @@ class SearchWidgets extends StatelessWidget {
               ),
               Flexible(
                 child: Container(
-                  child: Column(children: [
-                    Text(
-                      'sadasdasdasdasdasdsdsdassfdsfsdfsdfsdfsdfsd',
-                      overflow: TextOverflow.clip,
-                    ),
-                    Text(
-                      'sadasdasdasdasdasdsdsdassfdsfsdfsdfsdfsdfsd',
-                      overflow: TextOverflow.clip,
-                    )
-                  ]),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15.0, top: 8),
+                          child: Text(
+                            product.name,
+                            overflow: TextOverflow.clip,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15.0, top: 8),
+                          child: Text(
+                            product.description,
+                            overflow: TextOverflow.clip,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 13.0, top: 8),
+                          child: Text.rich(TextSpan(
+                            children: [
+                              WidgetSpan(
+                                  child: Icon(
+                                Icons.attach_money_outlined,
+                                size: 20,
+                                color: Colors.green,
+                              )),
+                              WidgetSpan(child: Text(product.price.toString())),
+                              WidgetSpan(child: Text('  تومان'))
+                            ],
+                          )),
+                        ),
+                      ]),
                 ),
               )
             ]),
           ),
         );
       },
-      itemCount: 2,
+      itemCount: products.length,
     );
   }
 }
