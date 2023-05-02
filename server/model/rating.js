9cc0b52786d693095970080237abf92db6ee01d5
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-05-02 23:58:55 +0330

    rating model done

--- server/model/rating.js
+++ server/model/rating.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 
 const ratingSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    rate: {
+        type: Number,
+        required: true,
+    },
 
 
-})
+
+
+});
+module.exports = ratingSchema;
