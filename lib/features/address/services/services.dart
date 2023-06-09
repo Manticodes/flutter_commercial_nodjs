784803dd5de43f14412760f694a0be6e2c7fe7ee
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-06-09 21:07:09 +0330

    fix post request of  address

--- lib/features/address/services/services.dart
+++ lib/features/address/services/services.dart
@@ -3,6 +3,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_commercial_nodjs/logic/bloc_user/user_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,26 +12,27 @@ import '../../../constants/error_handle.dart';
 import '../../../constants/global_variable.dart';
 
 class AddressServices {
-  void addAdress(String address) {
-    Future<bool> sellProduct(String adress, BuildContext context) async {
-      try {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        String? token = prefs.getString('x-auth-token');
+  void addAdress(String address, BuildContext context) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
 
-        http.Response resp = await http.post(Uri.parse(uriAddAdress),
-            headers: {
-              'Content-Type': 'application/json; charset=UTF-8',
-              'x-auth-token': token!,
-            },
-            body: jsonEncode({'address': address}));
+      http.Response resp = await http.post(Uri.parse(uriAddAdress),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'x-auth-token': token!,
+          },
+          body: jsonEncode({'address': address}));
 
-        httpErrorHandle(response: resp, context: context, onSuccess: () {});
-        return true;
-      } catch (e) {
-        ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: Text(e.toString())));
-        return false;
-      }
+      httpErrorHandle(
+          response: resp,
+          context: context,
+          onSuccess: () {
+            context.read<UserBloc>().add(AddAddress(adrress: address));
+          });
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
     }
   }
 }
