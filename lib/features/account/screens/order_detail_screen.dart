722fa54c805869c0a1e53da44d1e74f222326001
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-07-08 22:06:29 +0330

    create order Screen and widgets

--- lib/features/account/screens/order_detail_screen.dart
+++ lib/features/account/screens/order_detail_screen.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commercial_nodjs/features/account/widget/order_product_list.dart';
 
 import '../../../model/order.dart';
 
 class OrderDetailScreen extends StatelessWidget {
-  const OrderDetailScreen({
+  OrderDetailScreen({
     Key? key,
     required this.order,
   }) : super(key: key);
   final Order order;
   static const String route = "/orderDetails";
+  RegExp reg = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+  String Function(Match) mathFunc = (Match match) => '${match[1]},';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Center(child: Text(order.products.toString())),
-    ));
+      appBar: AppBar(
+        title: const Text('Shopping Cart'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [OrderProductList(order: order)],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text.rich(TextSpan(
+                    children: [
+                      TextSpan(text: ' جمع کل'),
+                    ],
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold))),
+                Text(
+                    ' ${order.totalPrice.round().toString().replaceAllMapped(reg, mathFunc)} ')
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/account/widget/order_product_list.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+import '../../../model/order.dart';
+import '../../search/widget/stars.dart';
+
+class OrderProductList extends StatelessWidget {
+  OrderProductList({Key? key, required this.order}) : super(key: key);
+  final Order order;
+  RegExp reg = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+  String Function(Match) mathFunc = (Match match) => '${match[1]},';
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [
+          Column(
+              children: order.products.map((e) {
+            int quantity = e.quantity as int;
+            double avgStar = 0;
+
+            if (e.ratings != null) {}
+            var ratingList = e.ratings;
+            double totalRating = 0;
+            for (var i = 0; i < ratingList!.length; i++) {
+              totalRating += ratingList[i].rate;
+            }
+            if (totalRating != 0) {
+              avgStar = totalRating / ratingList.length;
+            }
+
+            var aPrice =
+                e.price.round().toString().replaceAllMapped(reg, mathFunc);
+
+            var totalPrice = (e.price * quantity)
+                .round()
+                .toString()
+                .replaceAllMapped(reg, mathFunc);
+            return Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Flexible(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const SizedBox(
+                            height: 3,
+                          ),
+                          Text(e.name,
+                              textAlign: TextAlign.right,
+                              softWrap: true,
+                              textDirection: TextDirection.rtl,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0, top: 5),
+                            child: Text.rich(
+                                textAlign: TextAlign.right,
+                                TextSpan(
+                                  children: [
+                                    const WidgetSpan(
+                                        child: Text(
+                                      '  تومان  ',
+                                      style: TextStyle(fontSize: 15),
+                                    )),
+                                    WidgetSpan(
+                                      child: Text(
+                                        aPrice,
+                                        style: const TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15.0, top: 5),
+                            child: Text(
+                              '${e.category} : دسته ',
+                              overflow: TextOverflow.clip,
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 14.0, top: 5),
+                            child: Stars(
+                              rating: avgStar,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.03,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        e.images[0],
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        height: MediaQuery.of(context).size.height * 0.25,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.03,
+                    ),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Divider(
+                    color: Color.fromARGB(190, 138, 143, 146),
+                  ),
+                )
+              ],
+            );
+          }).toList())
+        ]));
+  }
+}
