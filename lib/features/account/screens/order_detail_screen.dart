0b89045def9ca1f2e4f95d0afbc33bd2b856ecc9
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-07-17 21:42:01 +0330

    Update order_detail_screen.dart

--- lib/features/account/screens/order_detail_screen.dart
+++ lib/features/account/screens/order_detail_screen.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_commercial_nodjs/features/account/widget/order_product_list.dart';
+import 'package:flutter_commercial_nodjs/features/admin/services/admin_services.dart';
 import 'package:flutter_commercial_nodjs/features/auth/widgets/costum_button.dart';
 import 'package:intl/intl.dart';
 
 import '../../../logic/bloc_user/user_bloc.dart';
 import '../../../model/order.dart';
 
-class OrderDetailScreen extends StatelessWidget {
+class OrderDetailScreen extends StatefulWidget {
   OrderDetailScreen({
     Key? key,
     required this.order,
   }) : super(key: key);
   final Order order;
   static const String route = "/orderDetails";
+
+  @override
+  State<OrderDetailScreen> createState() => _OrderDetailScreenState();
+}
+
+class _OrderDetailScreenState extends State<OrderDetailScreen> {
   final RegExp reg = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+
   // ignore: prefer_function_declarations_over_variables
   final String Function(Match) mathFunc = (Match match) => '${match[1]},';
 
   @override
   Widget build(BuildContext context) {
-    var date = DateTime.fromMillisecondsSinceEpoch(order.orderedAt);
+    void changeOrderStatus(
+      int status,
+    ) {
+      AdminServices().changeOrderStatus(
+          context: context,
+          status: status + 1,
+          order: widget.order,
+          onSuccess: () {
+            setState(() {});
+          });
+    }
+
+    var date = DateTime.fromMillisecondsSinceEpoch(widget.order.orderedAt);
     var date24 = DateFormat('dd/MM/yyyy').format(date);
     return Scaffold(
       appBar: AppBar(
@@ -30,7 +50,7 @@ class OrderDetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            OrderProductList(order: order),
+            OrderProductList(order: widget.order),
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: ClipRRect(
@@ -48,7 +68,7 @@ class OrderDetailScreen extends StatelessWidget {
                             style: TextStyle(
                                 fontWeight: FontWeight.bold, fontSize: 15)),
                         Center(
-                          child: Text(order.address,
+                          child: Text(widget.order.address,
                               style: const TextStyle(
                                   fontWeight: FontWeight.bold, fontSize: 13)),
                         )
@@ -61,15 +81,15 @@ class OrderDetailScreen extends StatelessWidget {
             BlocBuilder<UserBloc, UserState>(
               builder: (context, state) {
                 return Stepper(
-                  currentStep: order.status,
+                  currentStep: widget.order.status,
                   steps: [
                     Step(
                         title: const Text('در حال پردازش'),
                         content: const Text(
                             'سفارش شما در حال پردازش می باشد لطفا شکیبا باشید',
                             textAlign: TextAlign.right),
-                        isActive: order.status >= 0,
-                        state: order.status > 0
+                        isActive: widget.order.status >= 0,
+                        state: widget.order.status > 0
                             ? StepState.complete
                             : StepState.indexed),
                     Step(
@@ -77,8 +97,8 @@ class OrderDetailScreen extends StatelessWidget {
                         content: const Text(
                             'سفارش شما پردازش شده است و در صف انتظار ارسال می باشد',
                             textAlign: TextAlign.right),
-                        isActive: order.status > 0,
-                        state: order.status > 1
+                        isActive: widget.order.status > 0,
+                        state: widget.order.status > 1
                             ? StepState.complete
                             : StepState.indexed),
                     Step(
@@ -86,16 +106,16 @@ class OrderDetailScreen extends StatelessWidget {
                         content: const Text(
                             'سفارش شما ارسال شده است و بزودی به دست شما می رسد',
                             textAlign: TextAlign.right),
-                        isActive: order.status > 1,
-                        state: order.status > 2
+                        isActive: widget.order.status > 1,
+                        state: widget.order.status > 2
                             ? StepState.complete
                             : StepState.indexed),
                     Step(
                       title: const Text('تحویل داده شده'),
                       content: const Text('سفارش شما با موفقیت تحویل داده شد',
                           textAlign: TextAlign.right),
-                      isActive: order.status > 2,
-                      state: order.status > 2
+                      isActive: widget.order.status > 2,
+                      state: widget.order.status > 2
                           ? StepState.complete
                           : StepState.indexed,
                     ),
@@ -146,7 +166,7 @@ class OrderDetailScreen extends StatelessWidget {
                     style:
                         TextStyle(fontSize: 15, fontWeight: FontWeight.bold))),
                 Text(
-                    ' ${order.totalPrice.round().toString().replaceAllMapped(reg, mathFunc)} ')
+                    ' ${widget.order.totalPrice.round().toString().replaceAllMapped(reg, mathFunc)} ')
               ],
             ),
           ],
