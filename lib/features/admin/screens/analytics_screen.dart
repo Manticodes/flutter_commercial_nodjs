b6e5dec2f84e894d3dd50b450639165a837f5daa
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-07-19 21:39:34 +0330

    Update analytics_screen.dart

--- lib/features/admin/screens/analytics_screen.dart
+++ lib/features/admin/screens/analytics_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AnalyticsScreen extends StatelessWidget {
+class AnalyticsScreen extends StatefulWidget {
   const AnalyticsScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AnalyticsScreen> createState() => _AnalyticsScreenState();
+}
+
+class _AnalyticsScreenState extends State<AnalyticsScreen> {
   @override
   Widget build(BuildContext context) {
     return Column();
