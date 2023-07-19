bf0819cde6a37fddc0c425eb617a4f4998c3d91e
Sina Zerehpoosh <sina.zrp@gmail.com>
2023-07-19 21:51:33 +0330

    Update analytics_screen.dart

--- lib/features/admin/screens/analytics_screen.dart
+++ lib/features/admin/screens/analytics_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commercial_nodjs/features/admin/services/admin_services.dart';
+
+import '../../../model/sales.dart';
 
 class AnalyticsScreen extends StatefulWidget {
   const AnalyticsScreen({Key? key}) : super(key: key);
@@ -8,8 +11,27 @@ class AnalyticsScreen extends StatefulWidget {
 }
 
 class _AnalyticsScreenState extends State<AnalyticsScreen> {
+  int? totalsales;
+  List<Sales>? saleList;
+  void getEarnings() async {
+    var earningData = await AdminServices().getEarnings(context);
+    totalsales = earningData['totalEarnings'];
+    saleList = earningData['sales'];
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return saleList == null || totalsales == null
+        ? const CircularProgressIndicator()
+        : Column(
+            children: [
+              Text(
+                '\$$totalsales',
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )
+            ],
+          );
   }
 }
